97a00b2dcb6c0e82531ff16ae0340378d85d9c4b
Christopher Swinford <30563995+swinforc@users.noreply.github.com>
2018-08-26 15:16:04 -0400

    Update Sensor_Rewrite.ino
    Removed the code related to saving the data to an SD card. This code is no longer necessary as we will implement a telemetry system soon.

--- Sensor_Rewrite.ino
+++ Sensor_Rewrite.ino
@@ -1,13 +1,17 @@
 /*
- * The purpose of this code is to provide a clean and easily readable prgram for collecting both temperature and hunidity data from 
- * sensors. This data will eventually be transmitted to a ground station computer, but for now the data needed to be saved on an SD card.
+ * The purrpose of this script is to provide an updated version of the systems software, which is centered around eliminating the SD card reader.
+ * This removal is being conducted based on the idea that the transmission and receiving of data from the sensors in the system will occur 
+ * wirelessly. Other than removing the SD card related lines of code, the code will remain about the same as the previous version of this 
+ * software.
+ * 
+ * Due to the lack of a telemetry system, additional software was added to save the data collected to a file on the computer instead
+ * of the SD card.
  * 
  * Author: Christopher Swinford
- * Date: May 29, 2018
- * Version: 1.0.0
+ * Date: August 25, 2018
+ * Version: 1.1.0
  */
 
-#include <SD.h> //Allows for reading and writing to SD cards 
 #include <SPI.h> //Serial data protocol used by microcontrollers for communicating with one or more peripheral devices quickly over short distances.
 #include <SoftwareSerial.h> //Allow serial communication on other digital pins of the Arduino, using software to replicate the functionality
 
@@ -15,36 +19,26 @@
 SoftwareSerial GPSModule(3, 1); //Rx, Tx //Sets up a software serial object
 int pos;
 int stringplace = 0;
-const int ledPin = 13;
+const int humidPin = A1;
+const int tempPin = A2;
 String nmea[15];
+char degreeSymb = 176;
 
 void setup() {
   Serial.begin(9600); //Sets the data rate in bits per second for serial data transmission. Rate = 9600 bits per second
   GPSModule.begin(9600); //Sets the speed (baud rate) for the serial communication.
-  Serial.print("Initializing SD card..."); //Prints to computer screen
+  Serial.print("Initializing Telemetry..."); //Prints to computer screen an initialization message
 
-  //If the SD card is not initialized for pin 4 show error 
-  if (!SD.begin(4)){ 
-    Serial.println("initialization failed!"); //Print failed message to the computer
-    return;
-    }
-    
-  Serial.println("initialization done."); //Print success message to the computer
-  File logFile = SD.open("LOG.csv", FILE_WRITE); //Opens a file on the SD card.
-  String header = "Time, Latitude, Hemisphere, Longitude, Hemishpere, Fix, Altitude(MSL/M), Altitude(WGS84/M), Temperature, Humidity"; //Define the string for the header of the file
+  //Add initialization of Telemetry here
+  Serial.println("Telemetry Initialization Complete"); //Print success message to the computer
   
-  //If the file exisits print info to both serial monitor and file 
-  if (logFile){
-    logFile.println(", , ,");
-    logFile.println(header); //Print the header on the first line of the document
-    logFile.close(); //Close the file on the SD card
-    Serial.println(header);  //Prints the header to the computer monitor
-    pinMode(ledPin, OUTPUT); //Set the mode of the ledPin to output
-    pinMode(7, INPUT); //Set the mode of pin 7 to input
-  }
+  String header = "Time, Latitude, Hemisphere, Longitude, Hemishpere, Fix, Altitude(MSL/M), Altitude(WGS84/M), Temperature [" + String(degreeSymb) + "C], Humidity"; //Define the string for the header of the file
+  Serial.println(header);  //Prints the header to the computer monitor
+  
+
 }
-  
-void loop(){  
+
+void loop() {
   GPSModule.flush(); //Waits for the transmission of outgoing serial data to complete
   
   //Get the number of bytes (characters) available for reading from the serial port.
@@ -71,33 +65,23 @@ void loop(){
     stringplace = 0; //Reset string position to 0
     pos = 0; //Reset position to 0
     
-//Temperature Reading
-    int tempvalue; //Create an integer variable for the temperature value
-    int ftemp;
-    tempvalue=analogRead(A2); //Read the analog port 2 and store the value in tempval
+//Temperature Sensor Reading
+    int tempvalue, ftemp; //Declare integer variables
+    float Voltage, temp; //Declare float variables
+    tempvalue = analogRead(tempPin); //Read the analog port 2 and store the value in tempval
     //Conversion for temperature based on the voltage from sensor
-    float Voltage = tempvalue*((5.0/1023)*10);
-    float temp = (-40+(Voltage*10));
-    ftemp = ((temp*1.8)+32);
+    Voltage = tempvalue*((5.0/1023)*10);
+    temp = (-40+(Voltage*10)); //Temperature in C
+    ftemp = ((temp*1.8)+32); //Temperature in F
     
-//Humidity Reading
-    int humvalue; //Create an integer variable for temperature
-    int htemp;
-    humvalue=analogRead(A1); //Read the analog port 1 and store the value in val
+//Humidity Sensor Reading
+    int humvalue = analogRead(humidPin);; //Create an integer variable for humidity and read the analog port 1 and store the value in var
     float HVoltage = humvalue*((5.0/1023)*100); //Conversion for humidity based on the voltage from sensor
 
-//Create data string for storing to SD card
-//CSV format
-    String dataString = String(nmea[0]) + ", " + String(nmea[1]) + ", " + String(nmea[2]) + ", " + String(nmea[3]) + ", " + String(nmea[4]) + ", " + String(nmea[5]) + ", " + String(nmea[8]) + ", " + String(nmea[10]) + ", " + String(temp) + ", " + String(HVoltage);
-
-//Open a file to write to 
-    File logFile = SD.open("LOG.csv", FILE_WRITE); //Opens a file to write to
-    //If the file exisits do the following
-    if (logFile){
-        logFile.println(dataString); //Print data to the file
-        logFile.close(); //Close the file
-      }
+//Display the collected data to the serial monitor
+    //Data format
+    String dataString = nmea[0] + ", " + nmea[1] + ", " + nmea[2] + ", " + nmea[3] + ", " + nmea[4] + ", " + nmea[5] + ", " + nmea[8] + ", " + nmea[10] + ", " + String(temp) + ", " + String(HVoltage);
     Serial.println(dataString); //Print data to the serial monitor
-    delay(500); //Brief delay 
+    delay(500); //Brief delay
   }
 }
